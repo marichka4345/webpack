63df5f7adc26450e038a32d7ea2552b8bea22f24
mtretiak <mtretiak@corelogic.com>
2018-03-30 13:21:11 +0300

    add env check for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
-const env = process.env.NODE_ENV
-
 const webpack = require('webpack');
 const path = require('path');
+
 const CssHotLoader = require('css-hot-loader');
 const autoprefixer = require('autoprefixer');
+
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackConfig = new HtmlWebpackPlugin({template: 'index.html'});
@@ -42,79 +42,87 @@ const productionCssLoaders = {
                 minimize: true
             }
         },
-        'postcss-loader'
+        {
+            loader: 'postcss-loader'
+        }
     ]
 };
 
-module.exports = {
-    devtool: 'source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                use: 'html-es6-template-loader',
-                exclude: /node_modules/
-            }, {
-                test: /\.handlebars$/,
-                use: 'handlebars-loader',
-                exclude: /node_modules/
-            }, {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', 'postcss-loader', 'sass-loader']
-                }),
-                exclude: /node_modules/
-            }, {
-                test: /\.less$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', 'postcss-loader', 'less-loader']
-                })
-            }, {
-                test: /\.styl$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', 'postcss-loader', 'stylus-loader']
-                })
-            }, {
-                test: /\.css$/,
-                use: env === 'production'
-                    ? ExtractTextPlugin.extract(productionCssLoaders)
-                    : ExtractTextPlugin.extract('style-loader', 'css-loader', 'postcss-loader')
-            }, {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: 'eslint-loader',
-                enforce: 'pre'
-            }, {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            }, {
-                test: /\.ts$/,
-                exclude: /node_modules/,
-                use: 'ts-loader'
-            }, {
-                test: /\.coffee$/,
-                exclude: /node_modules/,
-                use: 'coffee-loader'
+module.exports = env => {
+    return {
+        mode: 'none',
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    commons: { test: /node_modules/, name: "vendors", chunks: "all" }
+                }
             }
+        },
+        devtool: 'source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            compress: true
+        },
+        resolve: {
+            extensions: ['.ts', '.js']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.html$/,
+                    use: 'html-es6-template-loader',
+                    exclude: /node_modules/
+                }, {
+                    test: /\.scss$/,
+                    use: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: ['css-loader', 'postcss-loader', 'sass-loader']
+                    }),
+                    exclude: /node_modules/
+                }, {
+                    test: /\.less$/,
+                    use: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: ['css-loader', 'postcss-loader', 'less-loader']
+                    })
+                }, {
+                    test: /\.styl$/,
+                    use: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: ['css-loader', 'postcss-loader', 'stylus-loader']
+                    })
+                }, {
+                    test: /\.css$/,
+                    use: env && env.production
+                        ? ExtractTextPlugin.extract(productionCssLoaders)
+                        : ExtractTextPlugin.extract('style-loader', 'css-loader', 'postcss-loader')
+                }, {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: 'eslint-loader',
+                    enforce: 'pre'
+                }, {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: 'babel-loader'
+                }, {
+                    test: /\.ts$/,
+                    exclude: /node_modules/,
+                    use: 'ts-loader'
+                }, {
+                    test: /\.coffee$/,
+                    exclude: /node_modules/,
+                    use: 'coffee-loader'
+                }
+            ]
+        },
+        plugins: [
+            CleanWebpackConfig,
+            HtmlWebpackConfig,
+            autoprefixer,
+            HotModuleReplacementConfig,
+            UglifyJsConfig,
+            ExtractTextPluginConfig
         ]
-    },
-    plugins: [
-        CleanWebpackConfig,
-        HtmlWebpackConfig,
-        autoprefixer,
-        HotModuleReplacementConfig,
-        UglifyJsConfig,
-        ExtractTextPluginConfig
-    ]
+    }
 };
